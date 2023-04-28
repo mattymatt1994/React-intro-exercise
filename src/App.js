e0cbfb3b54eb9dd13d32a80f053d61dda67b80cb
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-04-28 12:05:52 -0400

    Finished Intro Exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React is kinda cool...eh</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,18 @@ function App() {
           Learn React
         </a>
       </header>
+<h1>Dad Jokes For Tyson</h1>
+      <p>
+        <ul>
+         <li> Why can't you hear a pterodactyl going to the bathroom? Because the
+          "p" is silent!</li>
+          <li>What do you call an angry carrot? A steamed veggie.</li>
+          <li>What did the triangle say to the circle? You're pointless.</li>
+        </ul>
+
+        
+        
+      </p>
     </div>
   );
 }
